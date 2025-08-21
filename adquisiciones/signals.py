a8ade1dc5bf0ae7db8ca4c338c83b2c74391cd7b
Mónica <mmacuba@gmail.com>
2025-08-20 23:14:51 -0400

    Actualización

--- adquisiciones/signals.py
+++ adquisiciones/signals.py
@@ -3,10 +3,27 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
 from utils.models import Notification
-from .models import DetallesAdquisicion, DetallesAdquisicionEnvase, DetallesAdquisicionInsumo
+from .models import DetallesAdquisicion, DetallesAdquisicionEnvase, DetallesAdquisicionInsumo, Adquisicion
 from utils.tasks import send_notification_email
 
-@receiver(post_save, sender=DetallesAdquisicion)
+@receiver(post_save, sender=Adquisicion)
+def notify_adquisition_arrival(sender, instance, created, **kwargs):
+    if created:  # Solo para nuevas adquisiciones
+        # Obtener grupos objetivo
+        target_groups = Group.objects.filter(name__in=["almaceneros", "direccion"])
+        # Crear notificaciones para cada usuario en esos grupos
+        for group in target_groups:
+            for user in group.customuser_set.all():
+                # Notificación en base de datos
+                Notification.objects.create(
+                    user=user,
+                    message=f"Nueva adquisición recibida: {instance}",
+                    link=f'/movimientos/recepcion/{instance.tipo_adquisicion}/{instance.id}/'  # Ir a página para realizar la entrada al almacén.  
+                )
+                # Enviar email (asíncrono recomendado)
+                #send_notification_email.delay(user.id, instance.id)  # Pasar IDs en lugar de objetos
+
+""" @receiver(post_save, sender=DetallesAdquisicion)
 def notify_mp_arrival(sender, instance, created, **kwargs):
     if created:  # Solo para nuevos productos
         # Obtener grupos objetivo
@@ -59,3 +76,4 @@ def notify_insumo_arrival(sender, instance, created, **kwargs):
                 )
                 # Enviar email (asíncrono recomendado)
                 #send_notification_email.delay(user.id, instance.id)  # Pasar IDs en lugar de objetos
+ """
