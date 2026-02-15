2ce5d5c9acaf2b5e5b0b3c81753777fe711db5ac
Mónica <mmacuba@gmail.com>
2026-02-14 22:01:16 -0500

    revisión desde el inicio

--- InsumosOtros/urls.py
+++ InsumosOtros/urls.py
@@ -5,9 +5,9 @@ from .views import *
 urlpatterns = [
     path('catalogo/', ListInsumosView.as_view(), name='list_insumos'),
     path('listar/', listInsumos, name='insumos_list'),
-    path('crear/', InsumoCreateView.as_view, name='insumo_crear'),
-    path('eliminar/<uuid:pk>/', DeleteInsumoView.as_view, name='insumo_eliminar'),
+    path('crear/', InsumoCreateView.as_view(), name='insumo_crear'),
+    path('eliminar/<uuid:pk>/', DeleteInsumoView.as_view(), name='insumo_eliminar'),
     path('insumo/<uuid:pk>/', get_insumo, name='get_insumo'),
-    path('importar/', CreateImportView.as_view, name='importar_insumos'),  # Ruta para ver detalles
-    path('actualizar/<uuid:pk>/', UpdateInsumosView.as_view, name='insumos_editar'),  # Ruta para ver detalles
+    path('importar/', CreateImportView.as_view(), name='importar_insumos'),  # Ruta para ver detalles
+    path('actualizar/<uuid:pk>/', UpdateInsumosView.as_view(), name='insumos_editar'),  # Ruta para ver detalles
 ]
--- envase_embalaje/tapa/forms.py
+++ envase_embalaje/tapa/forms.py
@@ -34,16 +34,12 @@ class UpdateTapaForm(forms.ModelForm):
 
     class Meta:
         model = Tapa
-        fields = ['codigo', 'nombre', 'color', 'descripcion']
+        fields = ['nombre', 'color', 'descripcion']
         widgets = {
-            'codigo': forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}),
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
-            #'material': forms.TextInput(attrs={'class': 'form-control'}),
         }
         labels = {
-            'codigo': 'Código',
             'nombre': 'Nombre',
             'descripcion': 'Descripción',
-            #'material': 'Material',
         }
